0deb8f75cf428b9291c1b3836535b81eba18b620
Damien Dorobek <damiandorobek@gmail.com>
2022-02-20 10:12:17 -0500

    finished part1b

---
+++ part1/src/App.js
@@ -0,0 +1,42 @@
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const Header = ({ course }) => (
+    <h1>{course}</h1>
+  )
+
+  const Content = ({ parts }) => (
+  Object.values(parts).map(({ name, exercises }) => <p>{name} {exercises}</p>)
+  )
+
+  const Total = ({ parts }) => (
+    <p>Number of exercises {Object.values(parts).reduce((total, part) => total + part.exercises, 0)}</p>
+  )
+  
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App;
